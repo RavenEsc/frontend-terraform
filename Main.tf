e026d2b2b8c7fcb9497d9e7fbf68514b73047aaa
Raven Spencer <107158921+RavenEsc@users.noreply.github.com>
2023-06-13 13:40:47 -0500

    uploaded bucket

--- Main.tf
+++ Main.tf
@@ -12,16 +12,3 @@ provider "aws" {
   region = var.reg
   profile = var.profilename
 }
-
-data "aws_iam_policy_document" "example" {
-  statement {
-    actions   = ["s3:ListAllMyBuckets"]
-    resources = ["arn:aws:s3:::*"]
-    effect = "Allow"
-  }
-  statement {
-    actions   = ["s3:*"]
-    resources = [aws_s3_bucket.buck.arn]
-    effect = "Allow"
-  }
-}
--- s3.tf
+++ s3.tf
@@ -1,4 +1,4 @@
-# the bucket
+# Create an S3 bucket resource with specified name and tags
 resource "aws_s3_bucket" "buck" {
   bucket = var.bucketname
 
@@ -7,7 +7,8 @@ resource "aws_s3_bucket" "buck" {
     Environment = "Dev"
   }
 }
-# the configs
+
+# Configure the website document and error pages for the S3 bucket
 resource "aws_s3_bucket_website_configuration" "config" {
   bucket = aws_s3_bucket.buck.id
 
@@ -19,50 +20,47 @@ resource "aws_s3_bucket_website_configuration" "config" {
     key = "error.html"
   }
 }
-# the main page html file
+
+# Upload the index.html file to the S3 bucket
 resource "aws_s3_object" "index" {
   bucket = var.bucketname
+  content_type = "text/html"
+  server_side_encryption = "AES256"
   key    = "index.html"
   source = "Web-Frontend/index.html"
+
+  depends_on = [ aws_s3_bucket.buck ]
 }
-# the error page html file
+
+# Upload the error.html file to the S3 bucket
 resource "aws_s3_object" "error" {
   bucket = var.bucketname
+  content_type = "text/html"
+  server_side_encryption = "AES256"
   key    = "error.html"
   source = "Web-Frontend/error.html"
+
+  depends_on = [ aws_s3_bucket.buck ]
 }
-# the stylesheet css file
+
+# Upload the style.css file to the S3 bucket
 resource "aws_s3_object" "csstyle" {
   bucket = var.bucketname
+  content_type = "text/css"
+  server_side_encryption = "AES256"
   key    = "style.css"
   source = "Web-Frontend/my-css-for-resume.css"
+
+  depends_on = [ aws_s3_bucket.buck ]
 }
 
-# resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
-#   bucket = aws_s3_bucket.buck.id
-#   policy = data.aws_iam_policy_document.example.json
-# }
+# Create the S3 bucket policy resource to give public access to the web page
+resource "aws_s3_bucket_policy" "pubilc-policy" {
+  bucket = aws_s3_bucket.buck.id
+  policy = templatefile("s3-policy.json", { bucket = var.bucketname })
+}
 
-# data "aws_iam_policy_document" "allow_access_from_another_account" {
-#   statement {
-
-#     principals {
-#       type        = "AWS"
-#       identifiers = [""]
-#     }
-
-#     actions = [
-#       "s3:GetObject",
-#       "s3:ListBucket",
-#     ]
-
-#     resources = [
-#       aws_s3_bucket.buck.arn,
-#       "${aws_s3_bucket.buck.arn}/*",
-#     ]
-#   }
-# }
-
+# Block public access to S3 bucket
 resource "aws_s3_bucket_public_access_block" "publicaccess" {
   bucket = aws_s3_bucket.buck.id
 
