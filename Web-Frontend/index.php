579566e8f620c99f24851513050b56fc853f0c84
Raven Spencer <107158921+RavenEsc@users.noreply.github.com>
2023-12-22 01:56:18 -0600

    replace s3 files with update files

---
+++ Web-Frontend/index.php
@@ -0,0 +1,5 @@
+<?php
+    $ip = $_SERVER['REMOTE_ADDR'];
+    echo $ip
+    // You can use the $ip variable wherever you need it in your HTML
+    ?>
--- s3.tf
+++ s3.tf
@@ -21,13 +21,13 @@ resource "aws_s3_bucket_website_configuration" "config" {
   }
 }
 
-resource "aws_s3_object" "test" {
-  for_each = fileset("${var.s3_upload_dir}/", "**")
-  bucket = var.bucketname
-  key = each.key
-  source = "${var.s3_upload_dir}/${each.value}"
-  etag = filemd5("${var.s3_upload_dir}/${each.value}")
-}
+# resource "aws_s3_object" "test" {
+#   for_each = fileset("${var.s3_upload_dir}/", "**")
+#   bucket = var.bucketname
+#   key = each.key
+#   source = "${var.s3_upload_dir}/${each.value}"
+#   etag = filemd5("${var.s3_upload_dir}/${each.value}")
+# }
 
 # # Upload the index.html file to the S3 bucket
 # resource "aws_s3_object" "index" {
